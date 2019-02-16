bf44aec25c2cb9928d140fabce8434bbf268186d
spacehamster <mail.spacehamster@gmail.com>
2019-02-16 11:33:14 +1100

    first commit

---
+++ GoblinRace/Main.cs
@@ -0,0 +1,60 @@
+﻿using UnityModManagerNet;
+using System;
+using System.Reflection;
+using System.Linq;
+using Kingmaker.Blueprints;
+using Kingmaker;
+using Kingmaker.Blueprints.Classes;
+
+namespace GoblinRace
+{
+    public class Main
+    {
+        public static UnityModManagerNet.UnityModManager.ModEntry.ModLogger logger;
+        [System.Diagnostics.Conditional("DEBUG")]
+        public static void DebugLog(string msg)
+        {
+            if (logger != null) logger.Log(msg);
+        }
+        public static void DebugError(Exception ex)
+        {
+            if (logger != null) logger.Log(ex.ToString() + "\n" + ex.StackTrace);
+        }
+        public static bool enabled;
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                logger = modEntry.Logger;
+                var harmony = Harmony12.HarmonyInstance.Create(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            } catch(Exception ex)
+            {
+                DebugError(ex);
+                throw ex;   
+            }
+            return true;
+        }
+        [Harmony12.HarmonyPatch(typeof(LibraryScriptableObject), "LoadDictionary")]
+        static class LibraryScriptableObject_LoadDictionary_Patch
+        {
+            static void Postfix()
+            {
+                try
+                {
+                    Main.DebugLog("Adding goblins");
+                    ref var races = ref Game.Instance.BlueprintRoot.Progression.CharacterRaces;
+                    var goblinRace = ResourcesLibrary.TryGetBlueprint<BlueprintRace>("9d168ca7100e9314385ce66852385451");
+                    if (races.Contains(goblinRace)) return;
+                    var length = races.Length;
+                    Array.Resize(ref races, length + 1);
+                    races[length] = goblinRace;
+
+                } catch(Exception ex)
+                {
+                    Main.DebugError(ex);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Kingmaker Goblin Race Mod
+Unlock goblins as a playable race for the game Pathfinder Kingmaker.
+For goblin portraits, see [Avalonica's Goblin Portrait Pack﻿](https://www.nexusmods.com/pathfinderkingmaker/mods/21).
+
+## Install
+1. Download and install Unity Mod Manager﻿﻿ 0.13.0 or later
+2. Download the mod
+3. Extract the archive and put the mod folder into 'Mods' folder of the Game (\Steam\steamapps\common\Pathfinder Kingmaker\Mods).
+4. Run the game
+5. Goblins should now be selectable in the character creator.
+
+## Thanks
+to [JackQW](https://www.nexusmods.com/pathfinderkingmaker/users/958353) ﻿for creating the original goblin race mod in Kingmaker Mod Loader
