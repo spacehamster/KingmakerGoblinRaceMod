83b99bc72e8d34bf60dbb5e58d4c221caf061517
spacehamster <mail.spacehamster@gmail.com>
2020-10-10 21:58:57 +1100

    Fixed body texture on female goblins for 2.1.0 and newer. Moved to harmony 2.0

--- GoblinRace/Main.cs
+++ GoblinRace/Main.cs
@@ -10,49 +10,56 @@ using Kingmaker.UnitLogic.FactLogic;
 using Kingmaker.Localization.Shared;
 using UnityEngine;
 using System.IO;
-using Harmony12;
 using Kingmaker.Visual.CharacterSystem;
 using System.Collections.Generic;
-using static Harmony12.AccessTools;
+using HarmonyLib;
+using static HarmonyLib.AccessTools;
 
 namespace GoblinRace
 {
     public class Main
     {
-        public static UnityModManagerNet.UnityModManager.ModEntry.ModLogger logger;
         [System.Diagnostics.Conditional("DEBUG")]
-        public static void DebugLog(string msg)
+        public static void Log(string msg)
         {
             if (logger != null) logger.Log(msg);
         }
-        public static void DebugError(Exception ex)
+        public static void Error(Exception ex)
         {
-            if (logger != null) logger.Log(ex.ToString() + "\n" + ex.StackTrace);
+            if (logger != null) logger.Error(ex.ToString());
         }
+        public static void Error(string msg)
+        {
+            if (logger != null) logger.Error(msg);
+        }
+
+        public static UnityModManager.ModEntry.ModLogger logger;
         public static bool enabled;
+
         static bool Load(UnityModManager.ModEntry modEntry)
         {
             try
             {
                 logger = modEntry.Logger;
-                var harmony = Harmony12.HarmonyInstance.Create(modEntry.Info.Id);
+                var harmony = new Harmony(modEntry.Info.Id);
                 harmony.PatchAll(Assembly.GetExecutingAssembly());
             }
             catch (Exception ex)
             {
-                DebugError(ex);
+                Error(ex);
                 throw ex;
             }
             return true;
         }
-        [Harmony12.HarmonyPatch(typeof(LibraryScriptableObject), "LoadDictionary")]
+
+        [HarmonyPatch(typeof(LibraryScriptableObject), "LoadDictionary")]
         static class LibraryScriptableObject_LoadDictionary_Patch
         {
             static void Postfix()
             {
                 try
                 {
-                    Main.DebugLog("Adding goblins");
+                    Main.Log("Adding goblins");
                     ref var races = ref Game.Instance.BlueprintRoot.Progression.CharacterRaces;
                     var goblinRace = ResourcesLibrary.TryGetBlueprint<BlueprintRace>("9d168ca7100e9314385ce66852385451");
                     if (races.Contains(goblinRace)) return;
@@ -68,12 +75,12 @@ namespace GoblinRace
                 }
                 catch (Exception ex)
                 {
-                    Main.DebugError(ex);
+                    Main.Error(ex);
                 }
             }
         }
 
-        [Harmony12.HarmonyPatch(typeof(LocalizationManager), "LoadPack")]
+        [HarmonyPatch(typeof(LocalizationManager), "LoadPack")]
         static class LocalizationManager_CurrentLocale_SetterPatch
         {
             static void Postfix(Locale locale, ref LocalizationPack __result)
@@ -87,13 +94,13 @@ namespace GoblinRace
                 }
                 catch (Exception ex)
                 {
-                    Main.DebugError(ex);
+                    Main.Error(ex);
                 }
             }
         }
 
-        [Harmony12.HarmonyPatch(typeof(AssetBundle), "LoadFromFile", new Type[] { typeof(string) })]
-        static class AssetBundle_LoadFromFilePatch
+        [HarmonyPatch(typeof(AssetBundle), "LoadFromFile", new Type[] { typeof(string) })]
+        static class AssetBundle_LoadFromFile_Patch
         {
             static FieldRef<EquipmentEntity, List<Texture2D>> m_PrimaryRampsRef;
             static void Prepare()
@@ -106,13 +113,54 @@ namespace GoblinRace
                 {
                     var assetId = Path.GetFileName(path).Replace("resource_", "");
                     if (assetId != "e4b9c88f38026d440a12ae8ea148b8f3") return;
+                    Main.Log("Fixing Goblin texture");
                     var ee = __result.LoadAllAssets<EquipmentEntity>()[0];
                     m_PrimaryRampsRef(ee) = new List<Texture2D>();
                     ee.ColorsProfile = null;
                 }
                 catch (Exception ex)
                 {
-                    Main.DebugError(ex);
+                    Main.Error(ex);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(AssetBundle), "LoadFromFileAsync", new Type[] { typeof(string) })]
+        static class AssetBundle_LoadFromFileAsync_Patch
+        {
+            static FieldRef<EquipmentEntity, List<Texture2D>> m_PrimaryRampsRef;
+            static void Prepare()
+            {
+                m_PrimaryRampsRef = AccessTools.FieldRefAccess<EquipmentEntity, List<Texture2D>>("m_PrimaryRamps");
+            }
+            static void Postfix(string path, ref AssetBundleCreateRequest __result)
+            {
+                try
+                {
+                    var assetId = Path.GetFileName(path).Replace("resource_", "");
+                    if (assetId != "e4b9c88f38026d440a12ae8ea148b8f3") return;
+                    Main.Log("Registering goblin texture callback");
+                    __result.completed += OnAssetBundleLoad;
+
+                }
+                catch (Exception ex)
+                {
+                    Main.Error(ex);
+                }
+            }
+
+            private static void OnAssetBundleLoad(AsyncOperation obj)
+            {
+                if(obj is AssetBundleCreateRequest request)
+                {
+                    var bundle = request.assetBundle;
+                    var ee = bundle.LoadAllAssets<EquipmentEntity>()[0];
+                    m_PrimaryRampsRef(ee) = new List<Texture2D>();
+                    ee.ColorsProfile = null;
+                }
+                else
+                {
+                    Main.Error($"OnAssetBundleLoad Error: AsyncOperation is {obj.GetType()}");
                 }
             }
         }
